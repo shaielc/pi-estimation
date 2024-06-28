561b364ed2beebb1f718871711c452213aec97e2
Shy-El <shaielshmoel@gmail.com>
2024-06-28 16:54:54 +0300

    GS tasks

--- HW2/Gauss-Seidel/GS_tasks_parallel.c
+++ HW2/Gauss-Seidel/GS_tasks_parallel.c
@@ -1,7 +1,10 @@
 
 void gauss_seidel(int tsteps, int size, double **p) {
+      #omp pragma parallel
+      #omp pragma single
       for (int t = 0; t < tsteps; ++t) {
          for (int i = 1; i < size-1; ++i) {
+            #pragma omp task depend(in: p[i][j-1], p[i-1][j]) depend(out: p[i][j])
             for (int j = 1; j < size-1; ++j) {   
                //create a different task for each update
                p[i][j] = 0.25 * (p[i][j-1] + // left
@@ -12,4 +15,4 @@ void gauss_seidel(int tsteps, int size, double **p) {
          }
       }
    }
-}
+
