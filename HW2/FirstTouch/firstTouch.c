4f3e5869571139018b1b596d9d35725400a00570
Shy-El <shaielshmoel@gmail.com>
2024-06-28 16:42:52 +0300

    first touch

--- HW2/FirstTouch/firstTouch.c
+++ HW2/FirstTouch/firstTouch.c
@@ -2,7 +2,6 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define NTHREADS 12
 int main()
 {
     long int VectorSize = 2e9;
@@ -11,21 +10,23 @@ int main()
     double* c = (double*)malloc(VectorSize*sizeof(double));
     double d = 3.543;
 
-    omp_set_num_threads(NTHREADS);
-    //Step 1.a Initialization by initial thread only 
-   for (int j = 0; j < VectorSize; j++) { 
-       a[j] = 1.1-j;
-       b[j] = 2.2+j;
-       c[j] = 3.3*j;
-    }
-
-    //Step 1.b Initialization by all threads (first touch)
-    /*#pragma omp parallel for schedule(static)
-    for (int j = 0; j < VectorSize; j++) { 
-       a[j] = 1.1-j;
-       b[j] = 2.2+j;
-       c[j] = 3.3*j;
-    }*/
+    #ifndef SECTION_B
+       #pragma message("Building with Section A")
+       for (int j = 0; j < VectorSize; j++) { 
+           a[j] = 1.1-j;
+           b[j] = 2.2+j;
+           c[j] = 3.3*j;
+        }
+    #else
+        #pragma message("Building with Section B")
+    // Step 1.b Initialization by all threads (first touch)
+        #pragma omp parallel for schedule(static)
+        for (int j = 0; j < VectorSize; j++) { 
+           a[j] = 1.1-j;
+           b[j] = 2.2+j;
+           c[j] = 3.3*j;
+        }
+    #endif 
 
     double start_time = omp_get_wtime();
    //Step 2 Compute
@@ -38,5 +39,9 @@ int main()
      printf("OpenMP threads binding policy is %d \n", omp_get_proc_bind()); 
      // note that OpenMP Binding policy will be 3 if "close"
      // note that OpenMP Binding policy will be 4 if "spread"
-     printf("Number of threads %d \n", NTHREADS); 
+     int nthreads;
+     #pragma omp parallel
+     #pragma omp single
+     nthreads = omp_get_num_threads();
+     printf("Number of threads %d \n", nthreads);
 }
--- HW2/FirstTouch/run.sh
+++ HW2/FirstTouch/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 source /opt/intel/inteloneapi/setvars.sh > /dev/null 2>&1
-icx firstTouch.c -qopenmp -O0 -o bin/firstTouch 
-export OMP_PROC_BIND="close"
-#export OMP_PROC_BIND="spread"
+icx firstTouch.c -D${SECTION} -qopenmp -O0 -o bin/firstTouch
 ./bin/firstTouch
 
